b931650e85ca87384df8b4e89c1118f1488678e8
Aman-Gupta-404 <ag9139563@gmail.com>
2025-07-16 09:23:02 +0530

    added useer contorller

--- controller/users.js
+++ controller/users.js
@@ -1,17 +1,26 @@
 const Users = require("../model/users");
 
-const signupUser = (req, res) => {
+const signupUser = async (req, res) => {
   try {
     const userData = req.body;
-    const result = Users.create(userData);
+    const result = await Users.create(userData);
 
     if (result) {
-      res.status(201).send("User signup successful");
+      res.status(201).json({
+        error: false,
+        message: "User signup successful!",
+      });
     } else {
-      res.status(400).send("Error in singup");
+      res.status(400).json({
+        error: true,
+        message: "Error in signup!",
+      });
     }
   } catch (error) {
-    res.status(400).send("Error in singup");
+    res.status(400).json({
+      error: true,
+      message: error.message ? error.message : "Error in signup",
+    });
   }
 };
 
